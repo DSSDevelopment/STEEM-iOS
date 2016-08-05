5be6b4a5b5471973b4170824536045cd27f3cea8
derek.scott.sanchez@gmail.com <derek.scott.sanchez@gmail.com>
2016-08-05 14:24:52 -0500

    Ports prerequisites to new project with an application target instead of a framework.

---
+++ STEEMiOSDemo/Account.swift
@@ -0,0 +1,14 @@
+//
+//  Account.swift
+//  STEEMiOSDemo
+//
+//  Created by Derek Sanchez on 8/5/16.
+//  Copyright © 2016 Dramatech. All rights reserved.
+//
+
+import Foundation
+
+class Account
+{
+    
+}
