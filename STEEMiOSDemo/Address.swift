042e3b2646cdb05b985708b9268173fbad55d539
derek.scott.sanchez@gmail.com <derek.scott.sanchez@gmail.com>
2016-08-08 13:18:59 -0500

    Implements as much of Brainkey as possible without a proper PrivateKey class. Adds Address class stub.

---
+++ STEEMiOSDemo/Address.swift
@@ -0,0 +1,14 @@
+//
+//  Address.swift
+//  STEEMiOSDemo
+//
+//  Created by Derek Sanchez on 8/8/16.
+//  Copyright © 2016 Dramatech. All rights reserved.
+//
+
+import Foundation
+
+class Address
+{
+    
+}
